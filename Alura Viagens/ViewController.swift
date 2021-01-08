5d5458e18552da40d24dcfb55d36ca3be8ccccde
Fernando Kovaltchuk <fernando.kovaltchuk@gft.com>
2021-01-08 16:23:08 -0300

    Initial Commit

---
+++ Alura Viagens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alura Viagens
+//
+//  Created by Kovaltchuk, Fernando Cesar on 08/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
